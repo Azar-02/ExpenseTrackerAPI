bf80b4086aaa4db092983c2859711a7cd018a2bb
Mohamed Azarudeen <mailforazarudeen@gmail.com>
2026-07-25 13:33:31 +0530

    init

---
+++ app/schemas/user.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+
+
+class UserCreate(BaseModel):
+    name: str = Field(
+        min_length=3,
+        max_length=100,
+        description="Full Name"
+    )
+
+    email: EmailStr
+
+    password: str = Field(
+        min_length=8,
+        max_length=128,
+        description="User Password"
+    )
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
