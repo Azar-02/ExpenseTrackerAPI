09cd82906b7edae05d64bd2fabe5ffe1434def9f
Mohamed Azarudeen <mailforazarudeen@gmail.com>
2026-07-26 23:21:35 +0530

    minor refactor

--- app/models/expense.py
+++ app/models/expense.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy import Integer, String, ForeignKey, Numeric, Date
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from app.database import Base
+from app.database.base import Base
 
 class Expense(Base):
 
